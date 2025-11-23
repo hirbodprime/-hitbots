e32d427ca4647b30c9d88492174f61e04516fb10
hirbodprime <hirbodprime@gmail.com>
2025-11-24 00:26:05 +0330

    asdfasd fasdfasdf

--- hirbots/urls.py
+++ hirbots/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tetris/", include("tetris.urls", namespace="tetris")),
+]
+urlpatterns += [
+    re_path(
+        r"^tetris/music/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": os.path.join(settings.BASE_DIR, "tetris", "music"),
+        },
+    ),
 ]
